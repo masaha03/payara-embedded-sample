ccf2708dfa9297403317a5e88b69c3d189d63df5
Masahiro HAYASHI <mhayashi@mshackers.co.jp>
2015-10-04 17:58:15 +0900

    minor fixes

--- payara-embedded-sample-main/src/main/java/jp/mshackers/payara/embedded/sample/PayaraEmbedded.java
+++ payara-embedded-sample-main/src/main/java/jp/mshackers/payara/embedded/sample/PayaraEmbedded.java
@@ -6,23 +6,22 @@ import org.glassfish.embeddable.GlassFishProperties;
 import org.glassfish.embeddable.GlassFishRuntime;
 
 import java.io.InputStream;
-import java.net.URISyntaxException;
 
 public class PayaraEmbedded {
 
-    public static void main(String[] args) throws URISyntaxException, GlassFishException {
+    public static void main(String[] args) throws GlassFishException {
         GlassFishRuntime runtime = GlassFishRuntime.bootstrap();
 
         GlassFishProperties glassFishProperties = new GlassFishProperties();
 //        glassFishProperties.setPort("http-listener", 9080);
         String path = PayaraEmbedded.class.getResource("/domain").getPath();
-        System.out.println(path);
         glassFishProperties.setInstanceRoot(path);
 
         GlassFish glassFish = runtime.newGlassFish(glassFishProperties);
         glassFish.start();
 
-        InputStream inputStream = PayaraEmbedded.class.getClassLoader().getResourceAsStream("app.war");
+        InputStream inputStream = PayaraEmbedded.class.getClassLoader()
+                .getResourceAsStream("app.war");
 
         glassFish.getDeployer().deploy(inputStream, "--contextroot", "/");
     }
